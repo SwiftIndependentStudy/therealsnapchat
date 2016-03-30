0ed0b81aeaa615cd3c057ee0e4f685b823aedbe4
XCode <xcode@hsenatore-1809b.local>
2016-03-30 09:21:47 -0700

    Initial Commit

---
+++ Snapchat_1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Snapchat_1
+//
+//  Created by XCode on 3/30/16.
+//  Copyright © 2016 XCode. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Snapchat_1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Snapchat_1
+//
+//  Created by XCode on 3/30/16.
+//  Copyright © 2016 XCode. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
