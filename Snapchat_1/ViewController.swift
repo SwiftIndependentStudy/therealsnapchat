5a5348ef57e85d23bb405a8ce40be9cfecfcd7f8
Jack Li <jackprius@gmail.com>
2016-05-11 09:18:33 -0700

    Nothing

--- Snapchat_1/ViewController.swift
+++ Snapchat_1/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the    view.
+        // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear() {
